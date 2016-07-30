82a41ccf42798d975e233a7997bd71d87012291b
Lolomancer <toothpast.greetings@gmail.com>
2016-07-30 15:34:35 +0300

    + #2

--- Regular/JS2_HW3.js
+++ Regular/JS2_HW3.js
@@ -2,8 +2,16 @@
 
 //1.
 
-var t800 = /#+[\dA-Fa-f]+[\dA-Fa-f]/g;
+var t800 = /#+[\dA-F]+[\dA-F]/ig;
 
-var johnC = '#123456 and some other stuff like #679A3f, yahoo!';
+var sarahC = '#123456 and some other stuff like #679A3f, yahoo!';
 
-alert (johnC.match(t800));
+alert (sarahC.match(t800));
+
+//2.
+
+var t1000 = /\d+(\.\d)?/g;
+
+var johnC = '0 1 2 3.4 7.9 69 hastalavista 91';
+
+alert (johnC.match(t1000));
